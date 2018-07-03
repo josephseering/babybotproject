026df58e4a0d1b1b2f4eca83cafe60dc67c11fb4
josephseering <35943718+josephseering@users.noreply.github.com>
2018-07-03 11:23:34 -0400

    Add files via upload

---
+++ Settingsv2.py
@@ -0,0 +1,37 @@
+#HOST = "irc.twitch.tv"
+#HOST = "199.9.255.149"
+#HOST = "199.9.251.213"
+HOST = "irc.chat.twitch.tv"
+
+#PORT = 443
+PORT = 6667
+
+PASS = "oauth:cr61bagzasg86uu7vsjabq4k7myhmm"
+
+# Bot's username
+IDENT = "oghma2876"
+
+# List of channels to look at
+CHANNELLIST = ["gopconvention","mmond"]
+
+ROUNDS = 300
+
+ROUNDLENGTH = 28800
+
+
+
+
+
+
+
+#"nightblue3","hirple","amazhs","tsm_dyrus","dota2ruhub","dota2ruhub2","beyondthesummit","imaqtpie","roger9527","odpixel","mistakelolz","savjz","smitegame","rocketbeanstv","cohhcarnage","s1mpleof","hiko","gripex90","gnumme","scarletr0se","gafallen","yapyap30","sodapoppin","lirik","kolento","valkrin","dansgaming","overpow","giantwaffle","miramisu","summit1g","goldglove","itshafu","alinity","skumbagkrepo","aphromoo","streamerhouse","nyxeira","dexteritybonus","emilyispro","rewinside",
+
+#"bubbagame","ltzonda","poptart64","jendenise","circon",
+#"zeroxmercy","numotthenummy","srcavalon","spamfish","torres_uae1",
+#"swifty","etup","darkness_429","dota2fata","gamersoriginhs","enterelysium",
+#"zap55","m1k3rules","raizqt","daze4days","xblafans",
+#"kikettestar","c4mlann","hotted89","gamej06","dragon",
+#"minitrues","cyanide","jbrunzgaming","anzleon","crusade_gamer",
+#"purespam","heromarine","blatty","sickmotionlol","miss_rage",
+#"chainerfails","ikealyou","jodenstone","superjj102","elfinsox",
+#"santzo84","flavour_cs",
---
+++ settings.py
@@ -0,0 +1,11 @@
+
+API = {
+    'CLIENTID': '8o36vjmv1ss9ldm4uo63l2vr179fa4'
+}
+
+DATABASE = {
+        'NAME': 'streams',
+        'USER': 'streams',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+    }
